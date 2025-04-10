6463a1f1c6b50b57d323708fa94d23f5d0339ef1
1DT23CS009 Akanksh adi chandra <1dt23cs009@dsatm.edu.in>
2025-04-10 09:35:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="flex min-h-screen flex-col items-center justify-between p-24">
    
    with the text content:
    
        Total Income$0.00Total Expenses$0.00Balance$0.00Budget StatusOn Track
    
    Relevant files:
    
    And change it as follows:
    
    make the background more intresting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { DashboardOverview } from "@/components/DashboardOverview";
+import { BudgetGoals } from "@/components/BudgetGoals";
+import { ExpenseCategorization } from "@/components/ExpenseCategorization";
+import { ExpenseChatbot } from "@/components/ExpenseChatbot";
+import { SpendingCharts } from "@/components/SpendingCharts";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DashboardOverview />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-br from-blue-100 to-blue-50">
+      <ExpenseChatbot />
+      <ExpenseCategorization />
+      <SpendingCharts />
+      <BudgetGoals />
     </main>
   );
 }
+
